0cc2a7c7906bc0edae38f3caec9efb9b83857abd
Anderson Begossi <andersonbegossi@gmail.com>
2020-05-03 10:26:17 -0300

    Added localStorage mock

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -4,6 +4,10 @@ import { render, fireEvent } from '@testing-library/react'
 import TechList from '~/components/TechList';
 
 describe('TechList component', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  })
+
   it('should be able to add new tech', () => {
     const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
@@ -14,5 +18,19 @@ describe('TechList component', () => {
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByLabelText('Tech')).toHaveValue('');
 
+  });
+
+  it('should store techs in storage', () => {
+    let { getByTestId, getByLabelText, getByText, unmount } = render(<TechList />);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js'} });
+    fireEvent.submit(getByTestId('tech-form'));
+
+    unmount();
+
+    ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']))
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
   });
 });
